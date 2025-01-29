3acc5c61e730950e87152a3c3daacc098df59675
Md-Manir-hossain <mdmanirhossainb150@gmail.com>
2025-01-29 21:11:41 +0600

    Variable and Operator

---
+++ script.js
@@ -0,0 +1,47 @@
+
+
+
+// =========multiplication==========
+console.log ("This is our Second Javascript Class")
+
+let x = 300
+let y = 50
+let multiplication = x*y;
+console.log (multiplication);
+
+
+// =========sumation==========
+
+console.log ("This is our Second Javascript Class- 2")
+
+let a = 100
+let b = 80
+let sumation = a-b;
+console.log (sumation);
+
+// ========= Substraction ==========
+
+console.log ("This is our Second Javascript Class- 2")
+
+let a2 = 30
+let b2 = 10
+let Substraction = a2-b2;
+console.log (Substraction);
+
+
+// ========= Division ==========
+
+console.log ("This is our Second Javascript Class- 2")
+
+let a3 = 30
+let b3 = 10
+let Division = a3/b3;
+console.log (Division);
+
+// ==========Remainder=========
+
+let a5 = 49;
+let b5 = 5;
+
+let remainder = (49%5);
+console.log(remainder);
